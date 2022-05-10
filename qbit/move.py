8de508e06c4b00e0b93b172d9af87a2dbe19b4f4
Gehrman <catchchances@outlook.com>
2022-05-10 17:05:27 +0800

    下载完成转移位置

---
+++ qbit/move.py
@@ -0,0 +1,47 @@
+
+import datetime
+import sys
+import configparser
+import qbittorrentapi
+
+torrentName = sys.argv[1]
+
+
+def log(log):
+    with open("c:/app/btTools/qbit/log/qMove.log", 'a', encoding='utf-8') as logFile:
+        logFile.write("\n"+log)
+
+
+log("")
+log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+nowString = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+log("move the finish downloding media @ date and time:{}".format(nowString))
+log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+
+config = configparser.ConfigParser()
+log("read config.ini")
+config.read('C:/app/allConfig/qbit/qbitWeb.ini')
+log("connect qbit web ui")
+qbt_client = qbittorrentapi.Client(
+    host=config['conn']['host'],
+    port=config['conn']['port'],
+    username=config['conn']['username'],
+    password=config['conn']['password'],
+)
+
+
+for torrent in qbt_client.torrents_info():
+    if torrent.name == torrentName:
+        category = torrent.category
+        setPath = ""
+        if len(config['targetDir'][torrent.category]) > 0:
+            setPath = config['targetDir'][torrent.category.lower()]
+        if len(setPath) <= 0:
+            setPath = config['targetDir'][torrent.tag.lower()]
+        if len(setPath) <= 0:
+            setPath = config['targetDir']["other"]
+        log("move files to target dir. torrentName: '{}', targetDir: '{}'".format(
+            torrent.name, setPath))
+        torrent.setLocation(setPath)
+        log("done!")
